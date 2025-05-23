e058d09297080b1b72964c2eee406e762f927f3d
NomeFittizio <xxx@users.noreply.github.com>
2025-05-23 15:16:28 +0200

    Rimosse cartelle obsolete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,46 +4,45 @@ import { Casa } from "@prisma/client";
 import CardCasa from "@/components/CardCasa";
 import { allHouses, querySearch } from "@/action/casa.action";
 import CasaNonTrovata from "@/components/CasaNonTrovata";
+
+// app/page.tsx
 export default async function Home({
   searchParams,
 }: {
-  searchParams: Record<string, string>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const { nation, guests, rooms, bathrooms, startDate, endDate } = searchParams;
+  // Await the searchParams promise
+  const params = await searchParams;
+  
+  // parsing dei dati
+  const nation = params.nation as string || "";
+  const rooms = Number(params.rooms || 0);
+  const bathrooms = Number(params.bathrooms || 0);
+  const startDate = params.startDate as string;
+  const endDate = params.endDate as string;
 
-  const queryHouse = await querySearch(
-    nation,
-    Number(rooms),
-    Number(bathrooms),
-    startDate,
-    endDate
-
-  );
+  const queryHouse = await querySearch(nation, rooms, bathrooms, startDate, endDate);
   const allHouse = await allHouses();
-  if (!allHouse) return;
+  if (!allHouse) return null;
 
-  console.log("homepage", nation);
   return (
     <>
-      <>
-        <NavbarHome />
-
-        {!nation ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 mx-full p-4">
-            {allHouse.map((x: Casa) => (
-              <CardCasa key={x.id} casa={x} />
-            ))}
-          </div>
-        ) : queryHouse?.length === 0 ? (
-          <CasaNonTrovata />
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 mx-full p-4">
-            {queryHouse!.map((x: Casa) => (
-              <CardCasa key={x.id} casa={x} />
-            ))}
-          </div>
-        )}
-      </>
+      <NavbarHome />
+      {!nation ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 w-full p-4">
+          {allHouse.map((x: Casa) => (
+            <CardCasa key={x.id} casa={x} />
+          ))}
+        </div>
+      ) : queryHouse?.length === 0 ? (
+        <CasaNonTrovata />
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 w-full p-4">
+          {queryHouse!.map((x: Casa) => (
+            <CardCasa key={x.id} casa={x} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
